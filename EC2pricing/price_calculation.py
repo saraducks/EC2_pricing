ec80f2129ad32f12e1cec02dfd72ca3dd3c39417
saraducks <saranyadhakshina25@gmail.com>
2017-06-06 21:08:07 -0700

    added search_sku

--- EC2pricing/price_calculation.py
+++ EC2pricing/price_calculation.py
@@ -8,18 +8,40 @@ from EC2pricing.StaticParameters import (
 )
 
 class EC2Price:
-    # initialize the instance type and set the region
+    # initialize the instance_data using __init__.result and set the region to None
     def __init__(self):
         self.instance_data = result
         self.default_region = None
 
     def find_sku(self, **attributes):
+        # Stores the result if Match is True
+        Matched_attributes = set()
         # convert to camel case
         camelcase_res = self.camelcase_conversion(attributes)
         # for debugging and check if the result is in correct format
         print(camelcase_res)
         # search for sku using instance_data
-        print(self.instance_data)
+        #print(self.instance_data)
+        #boolean variable, set to False if there is no match
+        Match = True
+        '''
+        Returns the sku (unique number for the service/instance type)
+        iterate through the instance data and search for sku, productFamily
+        if the productFamily attributes matches with the user attributes then add them to
+        Matched_attributes set.
+
+        '''
+        for sku, productFamily in self.instance_data['products'].items():
+             attributes = productFamily['attributes']
+             for attr_name, attr_value in attributes.items():
+                 if attributes.get(attr_name) != attr_value:
+                     Match = False
+                     break
+             if Match == True:
+                 Matched_attributes.add(sku)
+        return Matched_attributes
+
+
 
 
     #convert the '_' to the camel case letters
@@ -27,8 +49,10 @@ class EC2Price:
         res = {}
         for attr_name, attr_value in attributes.items():
             if '_' in attr_name:
-                attr_name = ''.join(char.capitalize() for char in attr_name.split('_'))
-                attr_name = attr_name[0].lower()+attr_name[1:]
+                attr_name = attr_name.split('_')
+                attr_name = attr_name[0][0].lower()+attr_name[0][1:]+attr_name[1].capitalize()
+                # attr_name = ''.join(char.capitalize() for char in attr_name.split('_'))
+                # attr_name = attr_name[0].lower()+attr_name[1:]
             res[attr_name] = attr_value
         return res
 
