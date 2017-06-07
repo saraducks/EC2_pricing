0d6d66e4e6488122283f70adbf78b612ef605b61
saraducks <saranyadhakshina25@gmail.com>
2017-06-06 18:47:53 -0700

    added the ec2 JSON

--- EC2pricing/__init__.py
+++ EC2pricing/__init__.py
@@ -1,5 +1,11 @@
+import requests, json
+
 '''
  Get the user required AWS EC2 service along with few parameters
 
 '''
-print("hello world!!! :) ")
+# send the link request and parse them into the local JSON file
+url_response = requests.get("https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/index.json")
+print(url_response)
+
+print(url_response.json())
